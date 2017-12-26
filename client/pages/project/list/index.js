92bdef357ba57f647b69017a902bf3ea0ac2de9e
xugang <371801080@qq.com>
2017-12-26 22:59:29 +0800

    页面

--- client/pages/project/list/index.js
+++ client/pages/project/list/index.js
@@ -10,6 +10,28 @@ Page({
 		    {url:'../../../image/uway2.png'} ,    
 		    {url:'../../../image/ydr.png'} ,    
 		    {url:'../../../image/weixin2.png'}     
-	    ] 
+	    ],
+	    sortStatus: [{
+	    	name:'评分',sortUp:true
+	    },{
+	    	name:'日期',sortUp:true
+	    }]
+    },
+
+    getSort(e){
+    	var _this = this;
+    	var item = e.currentTarget.dataset.item;
+		var sortStatus = [];
+		_this.data.sortStatus.forEach(function(ele,i){
+			if(ele.name == item.name){
+				ele.sortUp = !item.sortUp
+			}
+			sortStatus.push(ele)
+
+		})
+		this.setData({
+			sortStatus: sortStatus
+		})
+
     }
 })
