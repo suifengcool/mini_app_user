b6260f0f7d6ae18e87b26bb3dacfb9b29d967e87
xugang <371801080@qq.com>
2017-12-27 22:50:10 +0800

    项目详情页

--- client/pages/project/detail/index.js
+++ client/pages/project/detail/index.js
@@ -5,12 +5,25 @@ var util = require('../../../utils/util.js')
 
 Page({
     data: {
-        id: ''
+        id: '',
+        currentTab: '1',
+        pics:[    
+            {url:'../../../image/uway1.png'} ,    
+            {url:'../../../image/uway2.png'} ,    
+            {url:'../../../image/ydr.png'} ,    
+            {url:'../../../image/weixin2.png'}     
+        ],
     },
 
     onLoad: function(options){
 		this.setData({
 			id: options.id
 		})
+    },
+
+    changeTab (e){
+		this.setData({
+            currentTab : e.currentTarget.dataset.tab
+        })
     }
 })
